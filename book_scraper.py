efe56ce1ad582d2e8b5e0ce95a4c1bf4659824a8
Ahmed Abdulrahman <ahmedabdulrahman419@gmail.com>
2022-01-22 13:57:23 +0800

    finished collecting books details

--- book_scraper.py
+++ book_scraper.py
@@ -78,8 +78,123 @@ class BooksScraper:
 
         return books_urls
 
+    def get_book_details(self):
+        """get book details from books urls and save it to output.csv
+        """
+
+        # create output.csv file
+        output_cols = ['title', 'author(s)', 'year', 'edition',
+                                'publisher', 'language', 'pages', 'category(s)', 'ISBN_13', 'rating(5)']
+        BooksScraper.create_csv(*output_cols)
+
+        # get books urls
+        books_urls = self.get_books_urls()
+
+        # get book details
+        for url in tqdm(books_urls, desc='Scraping', unit='book'):
+            try:
+                book_html = requests.get(url).text
+                book_parsed_html = BeautifulSoup(book_html, 'lxml')
+                main_div = book_parsed_html.find(
+                    'div', {'class': 'col-sm-9'})
+            except Exception as e:
+                continue
+
+            else:
+                # book title
+                try:
+                    title = main_div.h1.text.strip()
+                except Exception as e:
+                    title = None
+
+                # book author
+                try:
+                    authors = main_div.find_all('a', {'itemprop': 'author'})
+                    if len(authors) == 1:
+                        author = authors[0].text.strip()
+                    else:
+                        author_list = []
+                        for i in range(len(authors)):
+                            author_list.append(authors[i].text.strip())
+                        author = ', '.join(author_list)
+                except Exception as e:
+                    author = None
+
+                # book rating
+                try:
+                    rating = float(main_div.find(
+                        'span', {'class': 'book-rating-interest-score'}).text)
+                except Exception as e:
+                    try:
+                        rating = float(main_div.find(
+                            'span', {'class': 'book-rating-interest-score none'}).text)
+                    except Exception as e:
+                        rating = None
+
+                # book category
+                try:
+                    category = main_div.find(
+                        'div', {'class': 'bookProperty property_categories'}).find(
+                        'div', {'class': 'property_value'}).a.text.strip()
+                except Exception as e:
+                    category = None
+
+                # book year
+                try:
+                    year = main_div.find(
+                        'div', {'class': 'bookProperty property_year'}).find(
+                        'div', {'class': 'property_value'}).text.strip()
+                except Exception as e:
+                    year = None
+
+                # book edition
+                try:
+                    edition = main_div.find(
+                        'div', {'class': 'bookProperty property_edition'}).find(
+                        'div', {'class': 'property_value'}).text.strip()
+                except Exception as e:
+                    edition = None
+
+                # book publisher
+                try:
+                    publisher = main_div.find(
+                        'div', {'class': 'bookProperty property_publisher'}).find(
+                        'div', {'class': 'property_value'}).text.strip()
+                except Exception as e:
+                    publisher = None
+
+                # book language
+                try:
+                    language = main_div.find(
+                        'div', {'class': 'bookProperty property_language'}).find(
+                        'div', {'class': 'property_value'}).text.strip()
+                except Exception as e:
+                    language = None
+
+                # book pages
+                try:
+                    pages = main_div.find(
+                        'div', {'class': 'bookProperty property_pages'}).find(
+                        'div', {'class': 'property_value'}).span.text.strip()
+                except Exception as e:
+                    pages = None
+
+                # book ISBN_13
+                try:
+                    ISBN_13 = main_div.find(
+                        'div', {'class': 'bookProperty property_isbn 13'}).find(
+                        'div', {'class': 'property_value'}).text.strip()
+                except Exception as e:
+                    ISBN_13 = None
+
+                # update output.csv file
+                scraping_results = {'title': title, 'author(s)': author, 'year': year, 'edition': edition,
+                                    'publisher': publisher, 'language': language, 'pages': pages, 'category(s)': category, 'ISBN_13': ISBN_13, 'rating(5)': rating}
+                BooksScraper.update_csv(**scraping_results)
+                # sleep for 1 sec to avoid server overloaded
+                time.sleep(1)
+
 
 if __name__ == '__main__':
     scraper = BooksScraper(args.search_keyword)
-    urls = scraper.get_books_urls()
-    print(urls)
+    scraper.get_book_details()
